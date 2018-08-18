1eed7508e90551e53be04d00db481183bab20068
Samuele Mattiuzzo <samumatt@gmail.com>
2018-08-18 18:57:16 +0100

    [wip] moved logic from main.py into the Game class

--- main.py
+++ main.py
@@ -1,165 +1,22 @@
+# -*- coding: utf-8 -*-
 try:
 
 	# pygame + general imports
-	import pygame, sys,os
+	import pygame, sys, os
 	from pygame.locals import *
-
-	# resource imports
-	from resources.config import *
-	from resources.levels import *
 
 	# main game classes imports
 	from main_game.game import *
-	from main_game.level import *
-	from main_game.player import *
-	from main_game.square import *
-	from main_game.gui import *
-	from main_game.system import *
 
 except ImportError as err:
 	print("couldn't load module. %s" % (err))
 	sys.exit(2)
 
-## GLOBAL VARIABLES
 
 # pygame init
 pygame.init()
-game = Game(all_levels=LEVELS)
 
-
-def next_level():
-	game.GUI.display_game(game.level, game.player)
-	game.GUI.SCREEN.blit(game.GUI.GAME_AREA, (0, 0))
-	pygame.display.flip()
-
-
-# checks for the new player move and returns the new position. false if it's not (player dies!)
-def new_valid_pos(player_pos, keyName, lmap):
-	valid = False
-	is_dead = False
-	pos = player_pos
-
-	if keyName == pygame.K_LEFT and player_pos[1]>0:
-		if lmap[player_pos[0]][player_pos[1]-1] != 0:
-			pos = (pos[0], pos[1]-1)
-			valid = True
-		else:
-			is_dead = True
-
-	if keyName == pygame.K_RIGHT and player_pos[1]<9:
-		if lmap[player_pos[0]][player_pos[1]+1] != 0:
-			pos = (pos[0], pos[1]+1)
-			valid = True
-		else:
-			is_dead = True
-
-	if keyName == pygame.K_UP and player_pos[0]>0:
-		if lmap[player_pos[0]-1][player_pos[1]] != 0:
-			pos = (pos[0]-1, pos[1])
-			valid = True
-		else:
-			is_dead = True
-
-	if keyName == pygame.K_DOWN and player_pos[0]<9:
-		if lmap[player_pos[0]+1][player_pos[1]] != 0:
-			pos = (pos[0]+1, pos[1])
-			valid = True
-		else:
-			is_dead = True
-
-	return valid, is_dead, pos
-
-
-
-def main():
-	#time is specified in milliseconds
-	#fixed simulation step duration
-	step_size = 500
-	#max duration to render a frame
-	max_frame_time = 100
-	now = pygame.time.get_ticks()
-
-	# first draw
-	next_level()
-
-	__cycle = True
-	valid = is_dead = False
-	tmp_pos = game.player.pos
-	pressed = ""
-
-	while(__cycle):
-		#handle events
-
-		for event in pygame.event.get():
-			if event.type == QUIT:
-				print("Bye!")
-				sys.exit()
-
-			elif event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_ESCAPE:
-					sys.exit()
-				else:
-					valid, is_dead, tmp_pos = new_valid_pos(
-						game.player.pos,
-						event.key,
-						game.level.design
-					)
-
-		if is_dead:
-			print("You touched a poisonous wall! You are dead!")
-			print("Your score is: %s" % str(game.player.moves))
-			print("Bye!")
-			sys.exit()
-
-		else:
-			if valid:
-				# actually moves the player
-				valid = False
-				game.GUI.update_game(
-					game.player, game.level,
-					tmp_pos
-				)
-
-				# reset - uncover
-				game.GUI.SCREEN.blit(game.GUI.GAME_AREA, (0, 0))
-				pygame.display.flip()
-
-				if game._check_end():
-					game.player.update_best_score(
-						game.level_id,
-						game.level.name,
-						game.player.moves
-					)
-					if game._check_has_more_levels():
-						game.next_level()
-					else:
-						print("YOU WON!")
-						print("You completed %s stages with a total of %s steps" % (str(game.level_id), str(game.player.moves)))
-						print("\nScores:")
-						scores = game.player.get_best_scores()
-						for i in scores:
-							print("%s: %s - %s steps" % (str(i), str(scores[i][0]), str(scores[i][1])))
-						__cycle = False
-
-		#get the current real time
-		T = pygame.time.get_ticks()
-
-		#if elapsed time since last frame is too longame.GUI...
-		if T-now > max_frame_time:
-			#slow the game down by resetting clock
-			now = T - step_size
-			#alternatively, do nothing and frames will auto-skip, which
-			#may cause the engine to never render!
-
-		#this code will run only when enough time has passed, and will
-		#catch up to wall time if needed.
-		while(T-now >= step_size):
-			#save old game state, update new game state based on step_size
-			now += step_size
-		else:
-			pygame.time.wait(10)
-
-		#render game state. use 1.0/(step_size/(T-now)) for interpolation
 
 if __name__ == "__main__":
-	main()
+	game = Game()
+	game.execute()
--- main_game/game.py
+++ main_game/game.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
 try:
 	import os
 	import pygame
+
+	from resources.levels import LEVELS
+
 	from pygame.locals import *
 	from main_game.level import *
 	from main_game.player import *
@@ -12,21 +16,22 @@ except ImportError as err:
 
 class Game:
 
-	def __init__(self, level_id=0, all_levels=[]):
-		self.player = None
-		self.level_id = level_id
-		self.level = None
-		self.levels = all_levels
-		self.GUI = None
+	def __init__(self, level_id=0):
+		# the Game class' attributes are all private
+		self.__level_id = level_id
+		self.__player = None
+		self.__level = None
+		self.__GUI = None
 
-		self.on_init()
+		self.__move = None  # used to store the current move
+		self.__running = False  # used to determine if the game is running or not
 
-	def on_init(self):
-		self.level = Level(self.level_id)
-		self.player = Player()
-		self.player.new_start(self.level.start)
+	def init(self):
+		self.__level = Level(self.__level_id)
+		self.__player = Player()
+		self.__player.new_start(self.__level.start)
 
-		self.GUI = Graphics()
+		self.__GUI = Graphics()
 
 	def start(self):
 		pass
@@ -34,24 +39,164 @@ class Game:
 	def end(self):
 		pass
 
+	def on_event(self, event):
+		if event.type == QUIT:
+			self.cleanup()
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				self.cleanup()
+			else:
+				self.__move = self._new_valid_pos(event.key)
+
+	def cleanup(self):
+		print("Bye!")
+		pygame.quit()
+
+	def execute(self):
+		self.init()
+
+		#time is specified in milliseconds
+		#fixed simulation step duration
+		#max duration to render a frame
+		step_size, max_frame_time = 500, 100
+		now = pygame.time.get_ticks()
+		self.__running = True
+
+		self._draw_level()
+
+		while(self.__running):
+			# main game loop
+			for event in pygame.event.get():
+				self.on_event(event)
+			self.update()
+
+			#get the current real time
+			T = pygame.time.get_ticks()
+
+			#if elapsed time since last frame is too longame.GUI...
+			if T-now > max_frame_time:
+				now = T - step_size  #slow the game down by resetting clock
+
+			#this code will run only when enough time has passed, and will
+			#catch up to wall time if needed.
+			while(T-now >= step_size):
+				now += step_size  #save old game state, update new game state based on step_size
+			else:
+				pygame.time.wait(10)
+
+		self.cleanup()
+
 	def update(self):
-		pass
+		if self.__move:
+			valid, tmp_pos = self.__move
+			self.__cycle = valid
+			if not valid:
+				self._handle_death()
+			else:
+				self._draw_move(tmp_pos)
+				self._handle_move()
 
-	def next_level(self):
-		self.level_id += 1
-		self.level = Level(self.level_id)
+	# internal utilities
+	def _new_valid_pos(self, key_pressed):
+		valid = False
+		pos = x, y = self.__player.pos
 
-	def _get_settings(self):
-		pass
+		if key_pressed == pygame.K_LEFT and y>0:
+			if self.__level.design[x][y-1] != 0:
+				pos = (x, y-1)
+				valid = True
 
-	def _get_level(self):
-		pass
+		if key_pressed == pygame.K_RIGHT and y<9:
+			if self.__level.design[x][y+1] != 0:
+				pos = (x, y+1)
+				valid = True
 
-	def _get_player(self):
-		pass
+		if key_pressed == pygame.K_UP and x>0:
+			if self.__level.design[x-1][y] != 0:
+				pos = (x-1, y)
+				valid = True
+
+		if key_pressed == pygame.K_DOWN and x<9:
+			if self.__level.design[x+1][y] != 0:
+				pos = (x+1, y)
+				valid = True
+
+		return valid, pos
+
+	def _next_level(self):
+		self.__level_id += 1
+		self.__level = Level(self.level_id)
+		self.__player.new_start(self.__level.start)
+		self.__move = None
+		self._draw_level()
 
 	def _check_end(self):
-		return self.player.pos == self.level.end
+		return self.__player.pos == self.__level.end
 
 	def _check_has_more_levels(self):
-		return self.level_id + 1 == len(self.levels)
+		return self.level_id + 1 < len(LEVELS)
+
+	def _handle_death(self):
+		print("You touched a poisonous wall! You are dead!")
+		print("Your score is: %s" % str(self.__player.moves))
+		print("Bye!")
+		self.cleanup()
+
+	def _draw_move(self, pos):
+		self.__GUI.update_game(self.__player, self.__level, pos)
+
+		# reset - uncover
+		self.__GUI.SCREEN.blit(self.__GUI.GAME_AREA, (0, 0))
+		pygame.display.flip()
+
+	def _draw_level(self):
+		self.__GUI.display_game(self.__level, self.__player)
+
+		# reset - uncover
+		self.__GUI.SCREEN.blit(self.__GUI.GAME_AREA, (0, 0))
+		pygame.display.flip()
+
+	def _handle_move(self):
+		if self._check_end():
+			self._handle_end_level()
+			if self._check_has_more_levels():
+				self._next_level()
+			else:
+				self._handle_end_game()
+
+	def _handle_end_level(self):
+		self.__player.update_best_score(
+			self.__level_id,
+			self.__level.name,
+			self.__player.moves
+		)
+
+	def _handle_end_game(self):
+		print("YOU WON!")
+		print("You completed %s stages with a total of %s steps" % (str(self.__level_id), str(self.__player.moves)))
+		print("\nScores:")
+		scores = self.__player.get_best_scores()
+		for i in scores:
+			print("%s: %s - %s steps" % (str(i), str(scores[i][0]), str(scores[i][1])))
+		self.__running = False
+
+	# getters
+	@property
+	def player(self):
+		return self.__player
+
+	@property
+	def level_id(self):
+		return self.__level_id
+
+	@property
+	def level(self):
+		return self.__level
+
+	@property
+	def all_levels(self):
+		return LEVELS
+
+	@property
+	def gui(self):
+		return self.__GUI
